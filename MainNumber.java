2bf8d945893689a1562760aae82b668666b3ed88
Adledasouqi <adledasouqi12@outlook.com>
2024-03-10 14:47:25 +0300

    Accessing Java Class Methods
    Class Methods and Static Keywords

---
+++ MainNumber.java
@@ -0,0 +1,19 @@
+public class MainNumber {
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 7;
+
+        Number objNumber = new Number();
+        // Accessing and calling methods of Number class.
+        int minimumNumber = objNumber.minimum(number1, number2);
+        System.out.println("Minimum Value = " + minimumNumber);
+
+        int maxNumber = 20;
+        int minNumber = 17;
+       
+        MaximumNumber objMaximumNumber = new MaximumNumber();
+        
+        int maximumNumber = objMaximumNumber.findMaxNumber(maxNumber, minNumber);
+        System.out.println("Maximum Value: " + maximumNumber);
+    }
+}
---
+++ Number.java
@@ -0,0 +1,38 @@
+public class Number {
+    //Accessing Java Class Methods
+    public int minimum(int number1, int number2) {
+        int minNumber;
+        if(number1 > number2) {
+            minNumber = number2;
+        } else {
+            minNumber = number1;
+        }
+        // Return the manimum number
+        return minNumber;
+    }
+}
+
+class MaximumNumber {
+    // Instance variable
+    int maxNumber;
+
+    public int findMaxNumber(int maxNumber, int minNumber) {
+        if(this.maxNumber > minNumber) {
+            maxNumber = minNumber;
+        } else {
+            minNumber = maxNumber;
+        }
+        // Return the maximum number
+        return maxNumber;
+    }
+
+    /* 
+     * Note 🧑🏻‍💻
+     * this Keyword which is used as a reference to the object of the current class, 
+       with in an instance method or a constructor.
+       Using this you can refer the members of a class such as constructors, 
+       variables and methods.
+       Differentiate the instance variables from local variables if they have same names, 
+       within a constructor or a method.
+    */
+}
