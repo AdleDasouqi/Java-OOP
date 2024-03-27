b1f06985ad588e0c9b246342b8ab1afe5b637e5c
Adledasouqi <adledasouqi12@outlook.com>
2024-03-27 23:42:50 +0300

    Void Keyword
    Void keyword Example

---
+++ Void.java
@@ -0,0 +1,16 @@
+public class Void {
+
+    // Method with return type void
+    public static void printMessage() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        printMessage();
+    }
+    
+    /* 
+     * Note 🧑🏻‍💻
+     * The void keyword allows us to create methods which do not return a value. 
+    */
+}
