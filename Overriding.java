62c27c22d5ee215da64b4345740f8d8a5d159f3f
Adledasouqi <adledasouqi12@outlook.com>
2024-05-06 14:13:54 +0300

    Overriding
    Overriding Example

---
+++ Overriding.java
@@ -0,0 +1,54 @@
+public class Overriding {
+    public static void main(String[] args) {
+        // Animal reference and object
+        Animal animal = new Animal();
+        // Animal reference but Dog object
+        Animal dog = new Dog();
+
+        animal.move();
+        dog.move();
+    }
+}
+
+class Animal {
+    public void move() {
+        System.out.println("Animals can move");
+    }
+}
+
+class Dog extends Animal {
+   
+    @Override
+    public void move() {
+        // invokes the super class method
+        super.move();
+        System.out.println("Dogs can walk and run");
+    }
+}
+    
+    /*
+     * compile time error the Animal class doesn't have a method by the name of bark.
+    @Override
+    public void bark() {
+        System.out.println("Dogs can bark");
+    }
+    */
+
+/*
+ * Note: 🧑🏻‍💻
+ * Method overriding allows us to achieve run-time polymorphism and is used for writing specific definitions 
+   of a subclass method that is already defined in the superclass. 
+   The method is superclass and overridden method in the subclass should have the same declaration signature 
+   such as parameters list, type, and return type.
+
+ * Usage of Java Method Overriding
+   1. Method overriding is used for achieving run-time polymorphism.
+   2. Method overriding is used for writing specific definition of a subclass method (this method is known as the overridden method).
+ 
+* Method and Constructor Overriding
+  Each class has a different name and the constructor's name is the same as the class name. 
+  Thus, we cannot override a constructor as they cannot have the same name.
+
+* Method Overriding: Using the super Keyword
+  When invoking a superclass version of an overridden method the super keyword is used.
+*/
